c878877b9696fe63df951df17369571d891eaf4a
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-09-18 02:37:58 +0300

    Exercise№3 completed

---
+++ Exercise№3/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = GetNumber("Enter your number ");
+EvenOrNot(number);
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void EvenOrNot(int a)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine($"{a} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{a} -> нет");
+    }
+}
