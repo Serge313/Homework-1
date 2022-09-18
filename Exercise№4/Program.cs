75e08444ee01ddd0924e5402a5e8809526c5dcc5
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-09-18 03:05:50 +0300

    Exercise№4 completed

---
+++ Exercise№4/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = GetNumber("Enter your number ");
+PrintEvenNumbers(number);
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void PrintEvenNumbers(int a)
+{
+    int startNumber = 2;
+    while (startNumber <= a)
+    {
+        Console.Write($"{startNumber} ");
+        startNumber = startNumber + 2;
+    }
+}
